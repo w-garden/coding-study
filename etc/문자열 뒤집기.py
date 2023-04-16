f8431ddcaf5e2b6821f1ab51521bd3ac3e1be09a
w-garden <w.garden316@gmail.com>
2023-04-17 00:03:41 +0900

    문자열 뒤집기

---
+++ etc/문자열 뒤집기.py
@@ -0,0 +1,19 @@
+#문자열을 뒤집는 함수를 작성하라. 입력 값은 문자 배열이며 리턴 없이 리스트 내부를 직접 조작하라
+from typing import List
+
+s= ["h","e","l","l","o"]
+b= ["H","a","n","n","a","h"]
+c= "abcdefghijklmnopqrstuwxyz"
+#투 포인터를 이용한 스왑
+
+
+class reverseString:
+    def reverseString(self, s:List[str]) ->None:
+        left, right = 0, len(s)-1
+        while left < right:
+            s[left],s[right] = s[right],s[left]
+            left+=1
+            right-=1
+        print(s)
+r=reverseString()
+r.reverseString(list(c))
