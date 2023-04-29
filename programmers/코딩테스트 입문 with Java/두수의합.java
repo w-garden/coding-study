5927ff939ecc1f2a54ed85aa6d90416b064ac264
w-garden <w.garden316@gmail.com>
2023-04-29 14:20:57 +0900

    두 수의 합
    https://school.programmers.co.kr/learn/courses/30/lessons/120802?language=java

---
+++ programmers/코딩테스트 입문 with Java/두수의합.java
@@ -0,0 +1,10 @@
+public class 두수의합 {
+    public static void main(String[] args) {
+        System.out.println(solution(2,3));
+        System.out.println(solution(100,2));
+    }
+    public static int solution(int num1, int num2) {
+        return num1+num2;
+    }
+
+}
