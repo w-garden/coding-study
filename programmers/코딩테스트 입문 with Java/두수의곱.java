6801332c4f9f7f181ea02a2538095da7403d219d
w-garden <w.garden316@gmail.com>
2023-04-29 18:36:19 +0900

    두 수의 곱
    https://school.programmers.co.kr/learn/courses/30/lessons/120804

---
+++ programmers/코딩테스트 입문 with Java/두수의곱.java
@@ -0,0 +1,11 @@
+package 코딩테스트입문;
+
+public class 두수의곱 {
+    public static void main(String[] args) {
+        System.out.println(solution(3,4));
+        System.out.println(solution(27,19));
+    }
+    public static int solution(int num1, int num2) {
+        return num1*num2;
+    }
+}
