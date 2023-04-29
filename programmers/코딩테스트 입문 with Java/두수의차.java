eaa679960367fff3f40b73e1f40290aa9cf35673
w-garden <w.garden316@gmail.com>
2023-04-29 18:35:03 +0900

    두 수의 차
    https://school.programmers.co.kr/learn/courses/30/lessons/120803

---
+++ programmers/코딩테스트 입문 with Java/두수의차.java
@@ -0,0 +1,11 @@
+package 코딩테스트입문;
+
+public class 두수의차 {
+    public static void main(String[] args) {
+        System.out.println(solution(2,3));
+        System.out.println(solution(100,2));
+    }
+    public static int solution(int num1, int num2) {
+        return num1-num2;
+    }
+}
