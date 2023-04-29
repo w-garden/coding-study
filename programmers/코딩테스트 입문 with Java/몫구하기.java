d070002ff7289ecba4692f85132d5a62924263a8
w-garden <w.garden316@gmail.com>
2023-04-29 18:38:21 +0900

    몫구하기
    https://school.programmers.co.kr/learn/courses/30/lessons/120805

---
+++ programmers/코딩테스트 입문 with Java/몫구하기.java
@@ -0,0 +1,11 @@
+package 코딩테스트입문;
+
+public class 몫구하기 {
+    public static void main(String[] args) {
+        System.out.println(solution(10,5));
+        System.out.println(solution(7,2));
+    }
+    public static int solution(int num1, int num2) {
+        return num1/num2;
+    }
+}
