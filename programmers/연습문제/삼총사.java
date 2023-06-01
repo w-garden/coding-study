f7609992e76f0118c9dfad6240a9425e906b1af2
w-garden <w.garden316@gmail.com>
2023-06-01 22:37:16 +0900

    230601 연습문제

---
+++ programmers/연습문제/삼총사.java
@@ -0,0 +1,25 @@
+package 연습문제;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/131705
+ */
+public class 삼총사 {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{-2, 3, 0, 2, -5}));
+        System.out.println(solution(new int[]{-3, -2, -1, 0, 1, 2, 3}));
+        System.out.println(solution(new int[]{-1, 1, -1, 1}));
+    }
+
+    public static int solution(int[] number) {
+        int answer = 0;
+        for (int i = 0; i < number.length; i++) {
+            for (int j = i + 1; j < number.length; j++) {
+                for (int k = j + 1; k < number.length; k++) {
+                    if (number[i] + number[j] + number[k] == 0)
+                        answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
