e3bae8a0dd627175917e11f78dfd1a2fa0a680cc
w-garden <w.garden316@gmail.com>
2023-07-11 23:32:56 +0900

    230711 SQL연습문제

---
+++ programmers/SQL고득점 Kit/있었는데요 없었습니다.sql
@@ -0,0 +1,13 @@
+-- ANIMAL_INS 테이블은 동물 보호소에 들어온 동물의 정보를 담은 테이블
+-- ANIMAL_OUTS 테이블은 동물 보호소에서 입양 보낸 동물의 정보를 담은 테이블
+SELECT
+      A.ANIMAL_ID
+    , A.NAME
+FROM 
+      ANIMAL_INS A
+    , ANIMAL_OUTS B
+WHERE 
+     A.ANIMAL_ID = B.ANIMAL_ID
+ AND A.DATETIME > B.DATETIME
+ORDER BY
+    A.DATETIME
