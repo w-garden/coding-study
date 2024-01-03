fd8bb7630c8e47bef4b727cfb27d25b27bdb335e
w-garden <w.garden316@gmail.com>
2024-01-03 22:38:07 +0900

    do it 알고리즘 ch01 문제풀이

---
+++ doit_java_algorithm/algorithm/src/ch01/TriangleLB.java
@@ -0,0 +1,23 @@
+package ch01;
+
+import java.util.Scanner;
+
+public class TriangleLB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+
+        System.out.println("왼쪽 아래가 직각인 이등변삼각형을 출력합니다");
+
+        do {
+            System.out.print("몇 단 삼각형입니까? ");
+            n = sc.nextInt();
+        } while (n <= 0);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
